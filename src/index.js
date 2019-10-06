b0ff558976d2e2ecf603dfac71f7ad675933ee90
Eduard Ten <ten.edik220302@gmail.com>
2019-10-06 12:02:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let array = expression.split('*');
+  let counter = 0;
+  let fives = 0;
+  let twos = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+    
+    if (array[i][array[i].length - 1] == "!" &&
+        array[i][array[i].length - 2] == "!") {
+          
+        let num = Number(array[i].slice(0, array[i].length - 2));
+        
+        for (let j = num; j > 0; j -= 2) {
+          let k = j;
+
+          while (k % 5 == 0) {
+            k /= 5;
+            fives++;
+          }
+
+          while (k % 2 == 0) {
+            k /= 2;
+            twos++;
+          }
+        }
+    } else {
+
+      let num = Number(array[i].slice(0, array[i].length - 1));
+
+      for (let j = 1; j <= num; j++) {
+        let k = j;
+
+        while (k % 5 == 0) {
+          k /= 5;
+          fives++;
+        }
+
+        while (k % 2 == 0) {
+          k /= 2;
+          twos++;
+        }
+
+      }
+
+    }
+
+  }
+    return (twos < fives) ? twos : fives;
 }
